c3a02de363c8e5e6617c0e47566185a86bfe46ab
Michael Hazzard <miramardesign@gmail.com>
2019-09-21 07:45:14 -0300

    footer nav

--- src/app/components/global/navigation-footer/navigation-footer.component.ts
+++ src/app/components/global/navigation-footer/navigation-footer.component.ts
@@ -1,35 +1,31 @@
 import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
-import {WelcomeComponent} from '../../welcome/welcome.component';
+import { WelcomeComponent } from '../../welcome/welcome.component';
 import { getButtonImageColor } from 'src/app/data/content.data';
 @Component({
-    selector : 'navigation-footer',
-    templateUrl : './navigation-footer.component.html'
+  selector: 'navigation-footer',
+  templateUrl: './navigation-footer.component.html',
+  styleUrls: ['./navigation-footer.component.scss']
 })
-export class NavigationFooter  implements OnInit {
-    @Input('navigationPanes') navigationPaneData;
-    ngOnInit(){
+export class NavigationFooter implements OnInit {
+  @Input('navigationPanes') navigationPaneData;
 
+  color;
+  backbuttonsrc;
+  nextbuttonsrc;
+  constructor() {
+    this.color = getButtonImageColor();
+    if (this.color === 'skyblue') {
+      this.backbuttonsrc = 'assets/buttons/back-onco-path-btn.png';
+      this.nextbuttonsrc = 'assets/buttons/next-onco-path-btn.png';
+    } else if (this.color == 'yellow') {
+      this.backbuttonsrc = 'assets/buttons/back-ddri-btn.png';
+      this.nextbuttonsrc = 'assets/buttons/next-ddri-btn.png';
+    } else {
+      this.backbuttonsrc = 'assets/buttons/back-immuno-onc-btn.png';
+      this.nextbuttonsrc = 'assets/buttons/next-immuno-onc-btn.png';
     }
-    color;
-    backbuttonsrc;
-    nextbuttonsrc;
-    constructor() {
-        this.color = getButtonImageColor();
-        if(this.color=="skyblue")
-        {
-            this.backbuttonsrc='assets/buttons/back-onco-path-btn.png';
-            this.nextbuttonsrc='assets/buttons/next-onco-path-btn.png';
-        }
-        else if(this.color=="yellow")
-        {
-            this.backbuttonsrc='assets/buttons/back-ddri-btn.png';
-            this.nextbuttonsrc='assets/buttons/next-ddri-btn.png';
-        }
-        else
-        {
-            this.backbuttonsrc='assets/buttons/back-immuno-onc-btn.png';
-            this.nextbuttonsrc='assets/buttons/next-immuno-onc-btn.png';
-        }
-    }
-    generateArray = WelcomeComponent.prototype.generateArray
+  }
+  generateArray = WelcomeComponent.prototype.generateArray;
+
+  ngOnInit() {}
 }
